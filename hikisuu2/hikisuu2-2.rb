e5af5ede8f131fbd2ba66e09ebc18350942c7dfb
EXP-0011\guest1Day <otoka.japanesedrum@gmail.com>
2017-07-05 11:48:08 +0900

    クラス　引数２

---
+++ hikisuu2/hikisuu2-2.rb
@@ -0,0 +1,4 @@
+require "./hikisuu2.rb"
+
+kake=Kakezan.new
+kake.num(2,2,false)
---
+++ hikisuu2/hikisuu2.rb
@@ -0,0 +1,10 @@
+class Kakezan
+	def num(a,b=5,c=false)
+		total=a*b
+		if c==true
+			puts total**2
+		else
+			puts total
+		end
+	end
+end
