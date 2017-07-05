478b6b697ebbc70e29dc379363359871bab92f1f
EXP-0011\guest1Day <otoka.japanesedrum@gmail.com>
2017-07-05 16:49:31 +0900

    クラス継承連数

--- modori2-2.rb
+++ modori2-2.rb
@@ -3,3 +3,10 @@ profile=Profiles.new
 
 profile.set(12345,"garchomp","2000/01/01","treasure-town")
 profile.output
+puts
+puts
+
+testclass=Test.new
+testclass.set("","garchomp","10/17","","hello")
+testclass.outputs
+
--- modori2.rb
+++ modori2.rb
@@ -12,10 +12,20 @@ class Profiles
 		puts humans[3]
 	end
 end
+class Test < Profiles
+	def set(id,names,day,street,tests)
+		super(id,names,day,street)
+		@tests=tests
+	end
+	def outputs
+		print("#{@names}#{@day}#{@tests}")
+	end
+end
+
 
 =begin
-インスタンス変数を使う理由がようやく理解できてきました。
-インスタンス変数は、クラス内では関数を超えて使うことができるため、クラス内の
-グローバル変数みたいな（？）感じで利用できて便利ですね！
-
+クラス継承メモ：継承するならば、同じ関数内の引数はしっかり使ってあげなければならない。
+さもなければ継承せず手動で作ること（つまり継承するメリットは、作業を大きく減らせる、見やすくなる。などがある。
+また、super使う際、直下にほかの定義も書くのであれば、引数は省略してはならない。
+（ただしsuperして以降終わりであれば省略可能。)
 =end
