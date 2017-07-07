42ed73a988a8378eda38f707529b73946f6b90d1
EXP-0011\guest1Day <otoka.japanesedrum@gmail.com>
2017-07-07 12:39:45 +0900

    いろいろなクラス（loggerのみ使用（簡単すぎてごめんなさい…））

---
+++ files3.rb
@@ -0,0 +1,27 @@
+require 'logger'
+log=Logger.new('memo.log')	
+
+File.open("memo.log","w") do
+
+end
+
+log.debug(Time.new.strftime('開始　　%Y年 %m月 %d日 %H時 %M分 %S秒'))
+
+
+
+def fibo(n)
+	total=1
+	a,b=0,1
+	for i in 1..n do
+		total
+		total=a+b
+		a,b=b,a+b
+	end
+	puts total
+end
+fibo(300000)
+log.debug(Time.new.strftime('終了　　%Y年 %m月 %d日 %H時 %M分 %S秒'))
+
+x=File.read('memo.log')
+puts x
+	
