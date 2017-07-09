2b9c7ba3676c98215fdc55300eba25006469628b
garchomp-game <otoka.japanesedrum@gmail.com>
2017-07-10 00:48:25 +0900

    Add files via upload

---
+++ memo.sql
@@ -0,0 +1,15 @@
+-- create table pokemons
+-- (id int primary key,name varchar(255),pokemon varchar(255))
+insert into pokemons values
+(1,'おとか','ガブリアス'),
+(2,'さやのん','リザードン'),
+(3,'ブレイグ','ジュプトル'),
+(4,'クラレ','ガブリアス');
+
+/*
+create table テーブル名（列名）　--　作成 
+上insert intoする前にかならず実行する
+insert into テーブル名 列名（省略可能） values または
+insert into テーブル名 values
+（値を書く）
+*/
