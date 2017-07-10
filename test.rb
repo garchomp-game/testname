38a850bee7ce4c0908d56473515645b354b9ba71
EXP-0011\guest1Day <otoka.japanesedrum@gmail.com>
2017-07-10 14:01:04 +0900

    test.rb

---
+++ test.rb
@@ -0,0 +1,18 @@
+#! ruby -EWindows-31J
+# -*- mode:ruby; coding:Windows-31J -*-
+require 'mysql2'
+
+
+client = Mysql2::Client.new(:host => 'localhost', :username => 'garchomp', :password => 'matutani9613', :database => 'garchomp_db')
+
+# puts("DB connected!")
+# puts('plase profilesID,name,tell,age,birthday')
+# x=gets.chomp.split(',')
+# statement = client.prepare(%q{insert into profiles(pforileID,name,tell,age,birthday) VALUES(?,?,?,?,?)})
+# statement.execute(x[0],x[1].to_s,x[2].to_s,x[3],x[4].to_s)
+
+puts
+client.query(%q{select * from profiles; }).each do|i|
+  puts
+  puts i
+end
