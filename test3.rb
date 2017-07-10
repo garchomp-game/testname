f0981a6e72802525c4986bb8750856e88454e52f
EXP-0011\guest1Day <otoka.japanesedrum@gmail.com>
2017-07-10 14:37:11 +0900

    sql4

---
+++ test3.rb
@@ -0,0 +1,15 @@
+
+require 'mysql2'
+
+
+client = Mysql2::Client.new(:host => 'localhost', :username => 'garchomp', :password => 'matutani9613', :database => 'garchomp_db')
+
+# puts("DB connected!")
+# puts('plase profilesID,name,tell,age,birthday')
+# x=gets.chomp.split(',')
+# statement = client.prepare(%q{insert into profiles(pforileID,name,tell,age,birthday) VALUES(?,?,?,?,?)})
+# statement.execute(x[0],x[1].to_s,x[2].to_s,x[3],x[4].to_s)
+
+client.query(%q{select * from profiles where name LIKE '%茂%' ; }).each do|i|
+  puts i
+end
